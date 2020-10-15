17d375e1e36b08e9177ea69dc593f1b2ab3bccf6
Max Van Buskirk <cmax1018@gmail.com>
2020-10-15 18:17:36 -0400

    resolved build error

---
+++ build/precache-manifest.37556b9d333f7f4492f2141eeed41650.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1c701b4bc6c57ce27389606bd81c14c",
+    "url": "/breathingaiproduct/index.html"
+  },
+  {
+    "revision": "a0c7a7f505fceb91098c",
+    "url": "/breathingaiproduct/static/css/main.ce7b2312.chunk.css"
+  },
+  {
+    "revision": "3efdbabc42efb847ce01",
+    "url": "/breathingaiproduct/static/js/2.04cee281.chunk.js"
+  },
+  {
+    "revision": "5b571a43117d7de158db9e221eaa0225",
+    "url": "/breathingaiproduct/static/js/2.04cee281.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0c7a7f505fceb91098c",
+    "url": "/breathingaiproduct/static/js/main.685ff262.chunk.js"
+  },
+  {
+    "revision": "249b1a265980a6d71e2c",
+    "url": "/breathingaiproduct/static/js/runtime-main.43309425.js"
+  }
+]);
