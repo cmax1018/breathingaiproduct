3ce7518377ced37902cd988007859c269ec41010
Max Van Buskirk <cmax1018@gmail.com>
2020-10-14 21:16:41 -0400

    deploy branch

---
+++ precache-manifest.bab697e4a03ad57bbf4517f1e26fc883.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7b764bfea5fdd319d2dc920153e772a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29169208dd17ba629c14",
+    "url": "/static/css/main.d9acdd3a.chunk.css"
+  },
+  {
+    "revision": "d22d162af6aeaa36ebea",
+    "url": "/static/js/2.fd2ac486.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.fd2ac486.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29169208dd17ba629c14",
+    "url": "/static/js/main.8b308d9f.chunk.js"
+  },
+  {
+    "revision": "7a1d15780eb384e0448f",
+    "url": "/static/js/runtime-main.d506cdce.js"
+  }
+]);
